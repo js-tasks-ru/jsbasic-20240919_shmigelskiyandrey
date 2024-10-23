c4cfca7b1fa2ff40c85bd3afe5640073e54ff509
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-10-23 17:28:10 +0400

    8th module 3rd task

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,41 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return
+    };
+
+    let item = this.cartItems.find(item => item.product.id === product.id);
+
+    if (item == undefined) {
+      this.cartItems.push({product, count: 1})
+    } else {
+      item.count++
+    };
+
+    this.onProductUpdate(item);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let item = this.cartItems.find(item => item.product.id === productId);
+
+    item.count += amount;
+
+    this.cartItems = this.cartItems.filter(item => item.count !== 0);
+
+    this.onProductUpdate(item);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((totalCount, item) => totalCount + item.count, 0)
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((totalPrice, item) => totalPrice + item.product.price * item.count, 0)
   }
 
   onProductUpdate(cartItem) {
